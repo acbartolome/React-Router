92f7603e365e7a56f593e18a162488652fa5b5b0
adryanbartolome <adryanbartolome8123@gmail.com>
2024-05-20 18:11:52 -0700

    Completed the 'Navbar' tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import Red from "./components/Red";
+import Blue from "./components/Blue";
+import Home from "./components/Home";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,12 +11,16 @@ function App() {
   return (
     <>
       <div id="container">
-        {/* <h1>Hello React Router!</h1> */}
-        <div id="navbar">{/* navigation here */}</div>
+        <div id="navbar">
+          <Link to='/'>Home</Link>
+          <Link to='/blue'>Blue</Link>
+          <Link to='/red'>Red</Link>
+        </div>
         <div id="main-section">
           <Routes>
-            <Route path='/blue' element={<h1>Blue</h1>} />
-            <Route path='/red' element={<h1>Red</h1>} />
+            <Route path='/' element={<Home />}/>
+            <Route path='/blue' element={<Blue />} />
+            <Route path='/red' element={<Red />} />
           </Routes>
         </div>
       </div>
